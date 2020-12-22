e6b2d6addd4848870de99b1b0dd6e50c5d28747a
xuhuan he <xuhuanhe16@gmail.com>
2020-12-21 21:48:41 -0500

    94. Binary Tree Inorder Traversal

---
+++ tree/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [2,1]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        
+        while(cur != null || !stack.isEmpty()){
+            //put node into stack
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            //put stack into List
+            cur = stack.pop();//pop the stack and add it into my answer
+            ans.add(cur.val);
+            cur = cur.right;//set cur = the cur.right
+        }
+        return ans;
+    }
+}
